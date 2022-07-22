89a1643b9bde10936d6fd82d1bfa6d8c9ef4ef18
chi.nguyen@s3corp.com.vn <chi.nguyen@s3corp.com.vn>
2022-07-22 16:38:26 +0700

    commit

--- src/components/row/row.js
+++ src/components/row/row.js
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 
- import { firebaseDatabase } from "../../firebase/firebase";
+import { firebaseDatabase } from "../../firebase/firebase";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 function Row(props) {
   const [movies, setMovies] = useState([]);
 
   const { title, movieType } = props;
 
-  const leafRoot = 'movies';
-
+  const leafRoot = "movies";
+  const [trailerUrl, setTrailerUrl] = useState("");
   useEffect(() => {
     fetchMovies(movieType);
   }, []);
@@ -22,19 +24,41 @@ function Row(props) {
       }
     });
   };
-
+  const handleTrailer = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get('v'));
+          console.log("Trailer", setTrailerUrl(urlParams.get('v')))
+        })
+        .catch(() => console.log("unavai"));
+    }
+  };
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVar: {
+      autoplay: 1,
+    },
+  };
   return (
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
+            onClick={() => handleTrailer(movie)}
             className="row__poster row__posterLarge"
             src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
             alt={movie.original_name}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
